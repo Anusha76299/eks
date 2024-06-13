b296a197939f14e20d9b20f8c5b21f4dc818bacc
YelkuriRaghavendra <125492599+YelkuriRaghavendra@users.noreply.github.com>
2024-06-13 22:12:42 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -83,9 +83,9 @@ resource "aws_eks_node_group" "private-nodes" {
   instance_types = ["t3.small"]
 
   scaling_config {
-    desired_size = 1
-    max_size     = 5
-    min_size     = 0
+    desired_size = 2
+    max_size     = 2
+    min_size     = 1
   }
 
   update_config {
