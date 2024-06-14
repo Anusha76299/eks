71e8fc32f81ab241a3d88efc6be4e4cb9e65a4a7
YelkuriRaghavendra <125492599+YelkuriRaghavendra@users.noreply.github.com>
2024-06-14 11:17:01 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -128,15 +128,6 @@ resource "aws_route_table" "public" {
   }
 }
 
-  resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
-
-  route = {
-      cidr_block                 = "0.0.0.0/0"
-      gateway_id                 = aws_internet_gateway.igw.id
-    }
-}
-
 resource "aws_route_table_association" "private-us-east-1a" {
   subnet_id      = aws_subnet.private-us-east-1a.id
   route_table_id = aws_route_table.private.id
