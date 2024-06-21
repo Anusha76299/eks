a62cdaea4971d770ca646fc7526b078077e93f9a
Anusha76299 <111690644+Anusha76299@users.noreply.github.com>
2024-06-22 02:44:29 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -33,8 +33,8 @@ resource "aws_instance" "bookstack_instance" {
     sudo usermod -aG docker $USER
     sudo reboot
     sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-    sudo chmod +x /usr/local/bin/docker-compose
-    sudo mkdir ~/bookstack
+    sudo chmod +x /usr/local/bin/docker-compose 
+    sudo mkdir ~/bookstack && sudo chmod -R 777 ~/bookstack
   EOF
 
 }
