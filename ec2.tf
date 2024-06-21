a549896bca3388686032152b5876a4190e927bcb
YelkuriRaghavendra <125492599+YelkuriRaghavendra@users.noreply.github.com>
2024-06-21 23:53:33 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,7 @@
-#resource "tls_private_key" "pk" {
+
+
+
+  #resource "tls_private_key" "pk" {
  # algorithm = "RSA"
   #rsa_bits  = 4096
  #}
@@ -18,7 +21,12 @@ resource "aws_instance" "bookstack_instance" {
   ami           = "ami-08a0d1e16fc3f61ea"  # Specify your desired AMI
   instance_type = "t2.micro"          # Adjust instance type as needed
   key_name      = "ec2-demo" # Specify your SSH key pair name
-  security_groups = ["${aws_security_group.bookstack_sg.name}"]  # Ensure this security group allows inbound on port 8080
+  security_groups = ["${aws_security_group.bookstack_sg.name}"]  
+
+  provisioner "file" {
+    source      = "${path.module}/docker-compose.yml" 
+    destination = "~/docker-compose.yml"   
+  }
 
   user_data = <<-EOF
     #!/bin/bash
@@ -31,10 +39,9 @@ resource "aws_instance" "bookstack_instance" {
     sudo reboot
     sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
-    mkdir -p ~/bookstack
-    echo '${file("docker-compose.yml")}' > ~/bookstack/docker-compose.yml
-    cd ~/bookstack && sudo docker-compose up -d
+    cd ~ && sudo docker-compose up -d
   EOF
+
 }
 
 resource "aws_eip" "bookstack_eip" {
