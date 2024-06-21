732c866f67bccc6fdce2f67d57b352cef07082c6
YelkuriRaghavendra <125492599+YelkuriRaghavendra@users.noreply.github.com>
2024-06-21 17:20:06 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,12 +1,12 @@
-resource "tls_private_key" "pk" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
- }
+#resource "tls_private_key" "pk" {
+ # algorithm = "RSA"
+  #rsa_bits  = 4096
+ #}
 
-resource "aws_key_pair" "kp" {
-  key_name   = "myKey"       # Create a "myKey" to AWS!!
-  public_key = tls_private_key.pk.public_key_openssh
-}
+#resource "aws_key_pair" "kp" {
+ # key_name   = "myKey"       # Create a "myKey" to AWS!!
+  #public_key = tls_private_key.pk.public_key_openssh
+#}
 
 #   provisioner "local-exec" { # Create a "myKey.pem" to your computer!!
 #     command = "echo '${tls_private_key.pk.private_key_pem}' > ./myKey.pem"
@@ -17,7 +17,7 @@ resource "aws_key_pair" "kp" {
 resource "aws_instance" "bookstack_instance" {
   ami           = "ami-08a0d1e16fc3f61ea"  # Specify your desired AMI
   instance_type = "t2.micro"          # Adjust instance type as needed
-  key_name      = "mykey" # Specify your SSH key pair name
+  key_name      = "ec2-demo" # Specify your SSH key pair name
   security_groups = ["${aws_security_group.bookstack_sg.name}"]  # Ensure this security group allows inbound on port 8080
 
   user_data = <<-EOF
