66aa2c62c7980effcfd733a3d48c76f8a03bf38f
YelkuriRaghavendra <125492599+YelkuriRaghavendra@users.noreply.github.com>
2024-06-21 14:37:23 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -17,7 +17,7 @@ resource "aws_instance" "bookstack_instance" {
   ami           = "ami-08a0d1e16fc3f61ea"  # Specify your desired AMI
   instance_type = "t2.micro"          # Adjust instance type as needed
   key_name      = "ec2-demo" # Specify your SSH key pair name
-  security_groups = ["${aws_security_group.bookstack_sg.id}"]  # Ensure this security group allows inbound on port 8080
+  security_groups = ["${aws_security_group.bookstack_sg.name}"]  # Ensure this security group allows inbound on port 8080
 
   user_data = <<-EOF
     #!/bin/bash
