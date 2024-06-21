11f61576dfc60a34e8a1aa59967918bc1f7dab20
YelkuriRaghavendra <125492599+YelkuriRaghavendra@users.noreply.github.com>
2024-06-21 17:15:46 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -6,6 +6,7 @@ resource "tls_private_key" "pk" {
 resource "aws_key_pair" "kp" {
   key_name   = "myKey"       # Create a "myKey" to AWS!!
   public_key = tls_private_key.pk.public_key_openssh
+}
 
 #   provisioner "local-exec" { # Create a "myKey.pem" to your computer!!
 #     command = "echo '${tls_private_key.pk.private_key_pem}' > ./myKey.pem"
